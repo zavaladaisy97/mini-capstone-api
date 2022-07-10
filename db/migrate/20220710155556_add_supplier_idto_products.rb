4e2b3bca0280d643f4326d6c7f2ab3d4934e18c4
daisyalexandria <dazavala7@gmail.com>
2022-07-10 11:05:11 -0500

    created supplier model and migration, updated seeds file with suppliers

---
+++ db/migrate/20220710155556_add_supplier_idto_products.rb
@@ -0,0 +1,5 @@
+class AddSupplierIdtoProducts < ActiveRecord::Migration[7.0]
+  def change
+    add_column :products, :supplier_id, :integer
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,8 @@ product = Product.create(name: "charger", price: 150, image_url: "https://m.medi
 product = Product.create(name: "tech backpack", price: 75, image_url: "https://m.media-amazon.com/images/I/71wGtW0-NnL._AC_SX425_.jpg", description: "used to protect the laptop")
 
 product = Product.create(name: "headphones", price: 325, image_url: "https://www.bhphotovideo.com/images/images2500x2500/apple_mgyj3am_a_airpods_max_silver_1610234.jpg", description: "noise cancellation headphones, connect using bluetooth")
+
+
+supplier = Supplier.create(name: "Prada", email: "prada@test.com", phone_number: "3129999999")
+supplier = Supplier.create(name: "Miu Miu", email: "miumiu@test.com", phone_number: "7735555555")
+supplier = Supplier.create(name: "Fendi", email: "fendi@test.com", phone_number: "6309999999")
